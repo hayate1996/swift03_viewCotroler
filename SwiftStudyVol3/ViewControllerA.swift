c4a8f2a0f4b24fb3bb865507ab51e380d7d5f367
hiro.nagami <hiro.nagami@gmail.com>
2015-01-17 14:43:46 +0900

    add ViewControllerA and ViewControllerB

---
+++ SwiftStudyVol3/ViewControllerA.swift
@@ -0,0 +1,17 @@
+//
+//  ViewControllerA.swift
+//  SwiftStudyVol3
+//
+//  Created by Hiromasa Nagamine on 1/17/15.
+//  Copyright (c) 2015 hiro nagami. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ViweControllerA : UIViewController {
+    override func viewWillAppear(animated: Bool) {
+        self.view.backgroundColor = .yellowColor();
+        super.viewWillAppear(animated)
+    }
+}
---
+++ SwiftStudyVol3/ViewControllerB.swift
@@ -0,0 +1,17 @@
+//
+//  ViewControllerB.swift
+//  SwiftStudyVol3
+//
+//  Created by Hiromasa Nagamine on 1/17/15.
+//  Copyright (c) 2015 hiro nagami. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ViewControllerB : UIViewController {
+    override func viewWillAppear(animated: Bool) {
+        self.view.backgroundColor = .greenColor();
+        super.viewWillAppear(animated)
+    }
+}
